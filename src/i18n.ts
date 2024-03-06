13f65ad2196f04825e3f264f128c1e06a7bd8ed4
Kevin Hurtado <kevinhurtado002@gmail.com>
2024-03-06 01:33:01 -0500

    Update getCurrentHost variable in i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,8 @@ import i18n from 'i18next';
 import i18nBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : import.meta.env.VITE_API_URL;
+const getCurrentHost =
+  import.meta.env.MODE === 'development' ? 'http://localhost:5173' : import.meta.env.VITE_FRONTEND_URL;
 
 i18n
   .use(i18nBackend)
